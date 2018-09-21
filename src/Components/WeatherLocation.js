b5d65f18f84ce6a71bae363ad2050d1ba8d23455
mars1571 <miruiz@nativapps.co>
2018-09-21 10:44:10 -0500

    Creacion de subcomponentes e importacion en otros componentes

--- src/Components/WeatherLocation.js
+++ src/Components/WeatherLocation.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Location from './Location';
+import WeatherData from './WeatherData'
 const WeatherLocation = () => (
     <div>
-        "Weather Location"
+        <Location/>
+        <WeatherData/>
     </div>
 );
 
