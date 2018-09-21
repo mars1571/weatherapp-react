28c24ffb555324e961453bc08d82f3d9a9a13e5d
mars1571 <miruiz@nativapps.co>
2018-09-21 10:08:45 -0500

    inicializando nuevo proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import WeatherLocation from './Components/WeatherLocation';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <WeatherLocation/>
+      </div>
+    );
+  }
+}
+
+export default App;
