194ea2b4f57b9e71416fa2219faff12644776c90
mars1571 <miruiz@nativapps.co>
2018-09-21 11:48:24 -0500

    ejemplo template string ES6 en el campo "humidity"

--- src/Components/WeatherExtraInfo.js
+++ src/Components/WeatherExtraInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const WeatherExtraInfo = ({humidity, wind}) => (
     <div>
-        <span>{humidity + ' % - '}</span>
+        <span>{`${humidity} % | `}</span>
         <span>{wind + ' wind'}</span>
     </div>
 );
