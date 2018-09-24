6edd22e675a61bbd8f274d717e4d431c24d4ecc3
mars1571 <miruiz@nativapps.co>
2018-09-24 17:39:15 -0500

    agrega funcion automatica para consultar un dato y se coloca un anuncio de cargando para darle tiempo a que se renderizen los componentes

--- src/Components/WeatherLocation/index.js
+++ src/Components/WeatherLocation/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData'
-import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from './../../constants/weathers';
 import transformWeather from './../../services/transformWeather'
 import './styles.css';
 
@@ -9,20 +8,13 @@ const location = "Barranquilla,col";
 const api_key =  "806b60b3c80578a8022352e131b022a3";
 const api_weather = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
 
-const data1 = {
-    temperature: 20,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10m/s'
-};
-
 class WeatherLocation extends Component {
 
     constructor() {
         super();
         this.state = {
             city: 'Barranquilla',
-            data: data1
+            data: null
         };
     }
 
@@ -36,29 +28,15 @@ class WeatherLocation extends Component {
     };
     
     componentWillMount() {//se ejecuta luego del constructor, una unica vez
-        console.log("componentWillMount");
+        this.handleUpdateClick();
     }
-    
-    componentDidMount() {//se ejecuta luego del render, una unica vez
-        console.log("componentDidMount");
-    }
-
-    componentWillUpdate() {//se ejecuta antes del render, cada vez que actualice
-        console.log("componentWillUpdate");
-    }
-
-    componentDidUpdate() {//se ejecuta luego del render, cada vez que actualice
-        console.log("componentDidUpdate");
-    }
-    
 
     render = () => {
         const { city, data } = this.state;
         return (
             <div className="weatherLocationCont">
                 <Location city={city} />
-                <WeatherData data={data} />
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
+                {data ?<WeatherData data={data} /> : 'cargando...'}
             </div>
         );
     };
