c6840a1e5209b962bc7b1207d57027dd2cd58eaf
mars1571 <miruiz@nativapps.co>
2018-09-25 12:00:51 -0500

    mapeo de ciudades desde el componente LocationList

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities = [
+  "Barranquilla,col",
+  "Bogota,col",
+  "Madrid,es"
+];
 
 class App extends Component {
   render() {
     return (
         <MuiThemeProvider>
         <div className="App">
-          <LocationList />
+          <LocationList cities={cities} />
         </div>
         </MuiThemeProvider>
     );
